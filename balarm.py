63ed72b4e5a81c0100a345eed04476f402c3a24e
David Simek <david.simek1998@gmail.com>
2019-08-11 16:19:43 +0200

    initial commit

--- balarm.py
+++ balarm.py
@@ -2,9 +2,11 @@ import requests
 import time
 from win10toast import ToastNotifier
 
+
 toaster = ToastNotifier()
 
 
+# This function will return float number which will represent price of 1 btc in USD.
 def get_bitcoin_rate_usd():
 
     r = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
@@ -12,6 +14,12 @@ def get_bitcoin_rate_usd():
     return round(float(bitcoin_rate_usd))
 
 
+"""
+This function will return number which will represent price of 1 BTC in CZK
+
+bitcoin_rate = number representing price of 1 BTC in USD
+
+"""
 def btc_in_czk(bitcoin_rate):
 
     r = requests.get('https://api.exchangerate-api.com/v4/latest/USD')
@@ -21,6 +29,13 @@ def btc_in_czk(bitcoin_rate):
     return round(btc_czk_rate)
 
 
+"""
+This function will check if {bitcoin_rate} is more or less then {bitcoin_price}
+
+bitcoin_rate = number representing price of 1 BTC in USD
+bitcoin_price = number which represent price of bitcoin you would like to check 
+
+"""
 def check_bitcoin_rate(bitcoin_rate, bitcoin_price):
 
     if bitcoin_rate > bitcoin_price:
@@ -30,6 +45,7 @@ def check_bitcoin_rate(bitcoin_rate, bitcoin_price):
 
 
 while True:
+    # Flash notification with btc prices in USD and CZK every two minutes. 
 
     messages_btc = f'btc is {btc_in_czk(get_bitcoin_rate_usd()):,} K CZK'
     toaster.show_toast(f" btc is {get_bitcoin_rate_usd():,} K $", messages_btc, icon_path='.\\icons\\btc.ico', threaded=True, duration=5)
