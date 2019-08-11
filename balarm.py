5a0a52456e44442e971a6ac2cb44c25756e401b1
David Simek <david.simek1998@gmail.com>
2019-08-11 16:58:36 +0200

    comment

--- balarm.py
+++ balarm.py
@@ -6,6 +6,17 @@ from win10toast import ToastNotifier
 toaster = ToastNotifier()
 
 
+print(' Welcome in Balarm. Bitcoin notification app for windows')
+print('*********************************************************')
+print("")
+print('Choose how often do you want to get notification from Balarm')
+notification_time = int(input("Time in sec: "))
+print("")
+print('Choose which currency you want to display')
+print('1. CZK 2. USD 3. CZK+USD')
+currency = int(input("Type 1 or 2 or 3: "))
+
+
 # This function will return float number which will represent price of 1 btc in USD.
 def get_bitcoin_rate_usd():
 
@@ -45,8 +56,25 @@ def check_bitcoin_rate(bitcoin_rate, bitcoin_price):
 
 
 while True:
-    # Flash notification with btc prices in USD and CZK every two minutes. 
+    # Flash notification with btc prices in USD and CZK every two minutes.
 
-    messages_btc = f'btc is {btc_in_czk(get_bitcoin_rate_usd()):,} K CZK'
-    toaster.show_toast(f" btc is {get_bitcoin_rate_usd():,} K $", messages_btc, icon_path='.\\icons\\btc.ico', threaded=True, duration=5)
-    time.sleep(120)
+    if currency == 1:
+        # If user chose CZK
+
+        messages_btc = 'HODL'
+        toaster.show_toast(f" btc is {btc_in_czk(get_bitcoin_rate_usd()):,} K CZK", messages_btc, icon_path='.\\icons\\btc.ico', threaded=True, duration=5)
+        time.sleep(notification_time)
+
+    if currency == 2:
+        # If user chose USD
+
+        messages_btc = 'HODL'
+        toaster.show_toast(f" btc is {get_bitcoin_rate_usd():,} K $", messages_btc, icon_path='.\\icons\\btc.ico', threaded=True, duration=5)
+        time.sleep(notification_time)
+
+    if currency == 3:
+        # If user chose both
+
+        messages_btc = f'btc is {btc_in_czk(get_bitcoin_rate_usd()):,} K CZK'
+        toaster.show_toast(f" btc is {get_bitcoin_rate_usd():,} K $", messages_btc, icon_path='.\\icons\\btc.ico', threaded=True, duration=5)
+        time.sleep(notification_time)
